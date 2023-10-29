c76c437fce456ecfaf24c21fc06d1b9101e3b310
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-10-30 00:05:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//Q1
+// int [] num={50, -20, 0, 30, 40, 60, 10};
+// if(num[0]==num[num.length-1]){
+//     System.out.println("True");
+// }
+// else System.out.println("false");
+
+//Q2
+//int[] num2={1, 4, 17, 7, 25, 3, 100};
+//
+//        double sum = 0;
+//        for (int num : num2) {
+//            sum += num;
+//        }
+//        double average = sum / num2.length;
+//
+//        System.out.println("The average of the said array is: " + average);
+//        System.out.print("The numbers in the said array that are greater than the average are: ");
+//        for (int num : num2) {
+//            if (num > average) {
+//                System.out.print(num + " ");
+//            }
+//        }
+//        Q3
+// int [] num3={20, 30, 40,};
+//        if(num3[0]>num3[num3.length-1]){
+//            System.out.println("Larger value between first and last element:"+num3[0]);
+//        } else if (num3[0]<num3[num3.length-1]) {
+//            System.out.println("Larger value between first and last element:"+num3[num3.length-1]);
+//        }
+//     Q4
+//    int[] number={20, 30, 40};
+//     int first= number[0];
+//    number[0] = number[number.length-1];
+//    number[number.length-1] = first;
+////    System.out.println("New array after swapping the first and last elements: "+ Arrays.toString(number));
+//
+//        Q5
+//        int[] array = {2, 3, 40, 1, 5, 9, 4, 10, 7};
+//        int[] oddElements = new int[array.length];
+//        int[] evenElements = new int[array.length];
+//        int oddIndex = 0;
+//        int evenIndex = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                oddElements[oddIndex] = array[i];
+//                oddIndex++;
+//            } else {
+//                evenElements[evenIndex] = array[i];
+//                evenIndex++;
+//            }
+//        }
+//        int[] rearrangedArray = new int[array.length];
+//        for (int i = 0; i < oddIndex; i++) {
+//            rearrangedArray[i] = oddElements[i];
+//        }
+//        for (int i = 0; i < evenIndex; i++) {
+//            rearrangedArray[oddIndex + i] = evenElements[i];
+//        }
+//        System.out.println("Rearranged array:");
+//        for (int i = 0; i < rearrangedArray.length; i++) {
+//            System.out.print(rearrangedArray[i] + " ");
+//        }
+
+//
+//        Q6
+//        int[] array1 = {2, 3, 6, 6, 4};
+//        int[] array2 = {2, 3, 6, 6, 4};
+//
+//        if (array1.length != array2.length) {
+//            System.out.println("false");
+//
+//        }
+//        for (int i = 0; i < array1.length; i++) {
+//            if (array1[i] != array2[i]) {
+//                System.out.println("false");
+//            }
+//        }
+//        System.out.println("True");
+    }}
